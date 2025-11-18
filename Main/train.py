3f1ec374a7e823467b94e01594a06242665b47ca
haruukura <daniellin6656@gmail.com>
2025-11-17 19:41:36 -0500

    encode decode

--- Main/train.py
+++ Main/train.py
@@ -4,4 +4,16 @@ with open ('Input1_Shake.txt', 'r', encoding = 'utf-8') as f:
 print("length of the dataset is:", len(text))
 
 print(text[:1000])
-    
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+stoi = { ch:i for i,ch in enumerate(chars)}
+itos = { i:ch for i,ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
+
